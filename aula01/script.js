9c7a2d65ab465155124790521e4c96bf12c2509b
Daaassh <dr.polar099@gmail.com>
2024-02-01 10:05:20 -0300

    Aula01, terminada

---
+++ aula01/script.js
@@ -0,0 +1,13 @@
+/*
+Criando um console.log
+Apenas para testes.
+Aula 01.
+*/
+
+// Um comentario em linhaa.
+//console.log("Hello World!");
+
+// Consoles.
+console.log("Hello World!");
+console.log("Essa e uma mensagem do console");
+
