f17c11444a8452bac52384f033f8daf2c68726ab
Daaassh <dr.polar099@gmail.com>
2024-02-02 11:29:55 -0300

    Aula06, terminada

---
+++ aula06/index.js
@@ -0,0 +1,20 @@
+/*
+  Aula06 - JavaScript
+  Revisão
+*/
+
+// Criando as variáveis
+let n1 = 1
+let n2 = '1'
+
+// Comparando as variáveis
+let v1 = {
+  name: "Bruno"
+}
+
+let v2 = v1
+
+// Imprimindo as variáveis
+
+console.log(v1.name)
+console.log(v1 === v2)
